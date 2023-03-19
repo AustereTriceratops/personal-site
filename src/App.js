aa86d1762a98b6a3538ff04f58279f0297801ef7
AustereTriceratops <gatewaylevi@gmail.com>
2023-03-19 15:54:25 -0500

    more setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      kill suck destroy
+    <div className="App" onClick={incrementCount}>
+      {count}
     </div>
   );
 }
